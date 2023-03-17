931f56499a985416f6131bf0c74df31d44c737cb
balajigan <balaji_inf@yahoo.com>
2023-03-16 21:32:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Home Page"}
+
+@app.get("/products")
+def products():
+    return {"Name": "product1"}
+
+@app.get("/customers")
+def products():
+    return {"First Name": "John",
+             "Last Name": "Smith",
+             "Address": "1234 West RD"}
