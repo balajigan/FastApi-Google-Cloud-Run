e977d17faae7a995d1255a5028c8409b21ffc269
balajigan <balaji_inf@yahoo.com>
2023-03-21 19:23:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi import Response
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+        
 @app.get("/")
 def home():
     return {"message": "Home Page"}
@@ -16,3 +24,11 @@ def products():
     return {"First Name": "John",
              "Last Name": "Smith",
              "Address": "1234 West RD"}
+
+@app.get("/items/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Cheese", price=12.0),
+        Item(name="Catchup", price=19.0),
+    ]
+
