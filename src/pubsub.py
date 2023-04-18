79e3b321f4ec03a245802e8d69ad0e889ef2bc5a
balajigan <balaji_inf@yahoo.com>
2023-04-18 00:28:01 -0500

    Update pubsub.py

--- src/pubsub.py
+++ src/pubsub.py
@@ -5,7 +5,7 @@ class PubSub:
   def publishMessage(cls):
     publisher = pubsub_v1.PublisherClient()
     topic_name = 'projects/mytemporaryproject28490/topics/gcp-resource-topic'
-    publisher.create_topic(name=topic_name)
+    #publisher.create_topic(name=topic_name)
     future = publisher.publish(topic_name, b'My first message!', spam='eggs')
     future.result()
   # End of class
