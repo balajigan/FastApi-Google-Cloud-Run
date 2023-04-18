8684fe662e6bb761a86850e6cb3843a0e2756f76
balajigan <balaji_inf@yahoo.com>
2023-04-18 13:54:18 -0500

    Update pubsub.py

--- src/pubsub.py
+++ src/pubsub.py
@@ -5,14 +5,12 @@ class PubSub:
   def publishMessage(cls,resource_type,data_str):
     publisher = pubsub_v1.PublisherClient()
     topic_name = 'projects/mytemporaryproject28490/topics/gcp-resource-topic'
-    #publisher.create_topic(name=topic_name)
-    #future = publisher.publish(topic_name, b'My first message!', spam='eggs')
     data = data_str.encode("utf-8")
     future = publisher.publish(topic_name, data, resourceType=resource_type)
-    
     future.result()
 
   def callback(message):
+    print('The subscriber callback method is called')
     print(message.data)
     message.ack()
     
@@ -20,7 +18,6 @@ class PubSub:
     topic_name = 'projects/mytemporaryproject28490/topics/gcp-resource-topic'
     subscription_name = 'projects/mytemporaryproject28490/subscriptions/gcp-resource-topic-sub'
     with pubsub_v1.SubscriberClient() as subscriber:
-#      subscriber.create_subscription(name=subscription_name, topic=topic_name)
       future = subscriber.subscribe(subscription_name, cls.callback)
   
 # End of class  
