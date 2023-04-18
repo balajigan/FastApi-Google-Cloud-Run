455d4b5ff317bad15b02500972ae5f9650af141a
balajigan <balaji_inf@yahoo.com>
2023-04-18 00:01:35 -0500

    Update pubsub.py

--- src/pubsub.py
+++ src/pubsub.py
@@ -2,5 +2,10 @@ import os
 from google.cloud import pubsub_v1
 
 class PubSub:
-  
+  def publishMessage():
+    publisher = pubsub_v1.PublisherClient()
+    topic_name = 'projects/mytemporaryproject28490/topics/gcp-resource-topic'
+    publisher.create_topic(name=topic_name)
+    future = publisher.publish(topic_name, b'My first message!', spam='eggs')
+    future.result()
   # End of class
